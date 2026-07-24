66abfab14a91671d057ae7acfe460858a61198f7
hidjgr <hichemdjeghri1@gmail.com>
2026-07-24 14:38:59 +0200

    Defeat. Initial(x3) Commit(x3)™. Back to react-globe.gl.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Globe from 'react-globe.gl';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Globe />
+    </div>
+  );
+}
+
+export default App;
